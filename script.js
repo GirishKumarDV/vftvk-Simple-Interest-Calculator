6a857c6b79964ae62b74520b1d823e070d9ddb56
GirishKumarDV <gkdv19@gmail.com>
2022-04-13 01:34:54 +0530

    Final project submission as per given instructions

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function showRate() {
+    var rate = document.getElementById("rate");
+    var element = document.getElementById("rateValue");
+    element.innerHTML = parseFloat(rate.value) + "%";
+
 }
-        
+
+function computeInterest() {
+    let principle = parseFloat(document.getElementById("amount").value);
+    let rate = parseFloat(document.getElementById("rate").value);
+    let period = parseInt(document.getElementById("period").value);
+    let futureAmount = ((principle * rate * period) / 100).toFixed(2);
+    let currentYear = new Date().getFullYear();
+
+    if (!principle || principle <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("amount").focus();
+    } else {
+        var messageElement = document.getElementById("message");
+        messageElement.innerHTML = `If you deposit <span class="value">${principle}</span>,\nat an interest rate of <span class="value">${rate}%</span>.\nYou will recieve an amount of <span class="value">${futureAmount}</span>,\nin the year <span class="value">${currentYear+period}</span>`
+    }
+}
+
+function numYears() {
+    var option = ``;
+    for (let i = 1; i <= 20; i++) {
+        option += `<option value="${i}">${i}</option>`;
+    }
+    document.getElementById("period").innerHTML = option;
+}
